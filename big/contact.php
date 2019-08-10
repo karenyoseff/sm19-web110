98b10cc9455b4b4df4d317b16217936f654e9e6e
karenyoseff <52435963+karenyoseff@users.noreply.github.com>
2019-08-09 22:47:02 -0700

    Add files via upload

---
+++ big/contact.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+     <title>Web Developer Examples and Resources by Karen Garcia</title>
+  <meta charset="utf-8" />
+  <meta name="robots" content="noindex,nofollow" />
+  <meta name="viewport" content="width=device-width" />
+  <link rel="stylesheet" href="css/big.css" />
+  <link rel="stylesheet" href="css/nav.css" />
+  <!-- JQuery -->
+  <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+  <!-- Menu Maker Plug in -->
+  <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
+  <!-- Icon Library -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+     <link rel="stylesheet" href="css/form.css" /> 
+  <script src="js/script.js"></script>
+ </head>
+ <body>
+     
+   <main>
+    <header>
+     <p class="header"><a href="index.html"><span>Web Developer Examples and Resources by</span> Karen Garcia</a></p>
+        
+     <nav id="cssmenu">
+          <ul>
+             <li><a href="../index.html"><i class="fa fa-dot-circle-o" aria-hidden="true"></i> WEB110</a></li>
+             <li><a href="index.html"><i class="fa fa-fw fa-home"></i> BIG Website </a></li>
+             <li><a href="gallery.html"><i class="fa fa-picture-o" aria-hidden="true"></i> Light Box II Gallery</a></li> 
+             
+                <!-- drop down menu for research pages -->
+              <li><a href="#">Research Topics</a>
+                <ul>
+                    <li><a href="smo.html"><i class="fa fa-fw fa-thumbs-o-up"></i> SMO </a></li>
+                    <li><a href="accessibility.html"><i class="fa fa-fw fa-globe"></i> Accessibility</a></li>
+                </ul>
+             </li>
+
+             
+              <!-- drop down menu for google tool pages -->
+             <li><a href="#">Google Tools</a>
+                <ul>
+                   <li><a href="seo.html"><i class="fa fa-fw fa-desktop"></i> SEO</a></li>
+                   <li><a href="calendar.html"><i class="fa fa-fw fa-calendar"></i> Calendar</a></li>
+                   <li><a href="map.html"><i class="fa fa-fw fa-map-o"></i> Map</a></li>
+                </ul>
+             </li>
+             <li><a href="contact.php"><i class="fa fa-envelope-o" aria-hidden="true"></i>
+Contact Me</a></li>
+          </ul>
+
+    </nav>
+        
+     </header>
+       
+         <!-- START LEFT COLUMN -->
+    <section class="fullwidth"><h2>Contact Karen Garcia</h2>
+        
+         	<?php include 'includes/simple.php';?>
+	       <p class="clear-recaptcha"></p>
+        
+        
+    </section>
+    <!-- END LEFT COLUMN -->
+
+    
+     <footer>
+      <p><small>&copy; 2019 by <a href="contact.php">Contact Karen Garcia </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+    </footer>
+  </main>
+     
+     
+ </body>
+</html>
